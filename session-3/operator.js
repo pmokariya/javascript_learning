f8033c0d2b95abe4196285c742797e731e79a1ec
Pooja Mokariya <poojalm747@gmail.com>
2023-01-21 12:46:46 +0530

    javascript programs

---
+++ session-3/operator.js
@@ -0,0 +1,15 @@
+// var c = 1;
+// console.log(++c);//2 pre incr
+// console.log(c);//2
+
+var d = 1;
+console.log(d++); //1 post incr
+console.log(d); //2
+
+//String Methods: length, toUpperCase, toLowerCase, charAt, substring, slice, indexOf,
+//lastIndexOf, replace, concat, trim, split, join
+// Properties: length
+//Additional Methods: toString, valueOf, concat, repeat, search, match,
+//localeCompare, includes, startsWith, endsWith, padStart, padEnd, trimStart, trimEnd
+
+//temporary dead zone
